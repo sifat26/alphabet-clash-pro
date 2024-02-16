86e5ad77e3698666976016abecbb2eb6f26d7128
sifat26 <sifatict26@gmail.com>
2024-02-17 00:06:32 +0600

    action aded

---
+++ Scripts/alpha-clash.js
@@ -0,0 +1,9 @@
+function play(){
+    const homeSection=document.getElementById("home-screen");
+    //console.log(homeSection.classList);
+    homeSection.classList.add('hidden');
+    const gameSection=document.getElementById("playground");
+    gameSection.classList.remove('hidden');
+    
+
+}
