a2ee196bc22d837d2b2dc80c17f432fb4f5d4152
sifat26 <sifatict26@gmail.com>
2024-02-17 01:59:09 +0600

    action updated

--- Scripts/alpha-clash.js
+++ Scripts/alpha-clash.js
@@ -1,22 +1,17 @@
-function play(){
-    // const homeSection=document.getElementById("home-screen");
-    // //console.log(homeSection.classList);
-    // homeSection.classList.add('hidden');
-    // const gameSection=document.getElementById("playground");
-    // gameSection.classList.remove('hidden');
-    hideElementById('home-screen');
-    showElementById('playground');
-    continuousGame();
-    
-    
-    
-
+function play() {
+  // const homeSection=document.getElementById("home-screen");
+  // //console.log(homeSection.classList);
+  // homeSection.classList.add('hidden');
+  // const gameSection=document.getElementById("playground");
+  // gameSection.classList.remove('hidden');
+  hideElementById("home-screen");
+  showElementById("playground");
+  continuousGame();
 }
 function continuousGame() {
-
-    const alphabet = getRandomAlphabet();
-    console.log(alphabet);
-    const alphabetDiv = document.getElementById("random-alphabet");
-    alphabetDiv.innerText = alphabet;
-
+  const alphabet = getRandomAlphabet();
+  console.log(alphabet);
+  const alphabetDiv = document.getElementById("random-alphabet");
+  alphabetDiv.innerText = alphabet;
+  addBackgroundColor(alphabet);
 }
--- Scripts/utility.js
+++ Scripts/utility.js
@@ -7,10 +7,18 @@ function showElementById(elementID) {
   element.classList.remove("hidden");
 }
 function getRandomAlphabet() {
-  const alphabetString = "ABCDEFGHIJKLMNOPQRSTUVW";
+  const alphabetString = "abcdefghijklmnopqrstuvwxyz";
   const alphabets = alphabetString.split("");
   const randomNumber = Math.random() * 25;
   const randomIndex = Math.floor(randomNumber);
   const alphabet = alphabets[randomIndex];
   return alphabet;
+}
+function addBackgroundColor(elementID)
+{
+    const element = document.getElementById(elementID);
+    element.classList.add('bg-orange-400')
+
+    
+
 }
